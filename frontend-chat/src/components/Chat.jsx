64865b0b5db7f9ce52103a3e738b2d94471a7c93
Shreyans jain <shreyansjain1603@gmail.com>
2024-03-18 16:36:04 +0530

    loader added

--- frontend-chat/src/components/Chat.jsx
+++ frontend-chat/src/components/Chat.jsx
@@ -1,13 +1,20 @@
 import { useState, useContext, useRef, useEffect } from "react";
 import { ChatContext } from "../ChatContext";
 import axios from "axios";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import { SyncLoader } from "react-spinners";
+
+const loaderStyle = {
+  borderColor: "lightgray",
+};
 
 export default function Chat() {
-  const { chatState, setChatState,sentiment,setSentiment,setCc } = useContext(ChatContext);
+  const { chatState, setChatState, sentiment, setSentiment, setCc } =
+    useContext(ChatContext);
   const [message, setMessage] = useState("");
   const messagesEndRef = useRef(null);
-const[botResponse,setbotResponse]=useState("")
+  const [botResponse, setbotResponse] = useState("");
+  const [respBot, setRespBot] = useState(false);
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   };
@@ -16,16 +23,16 @@ const[botResponse,setbotResponse]=useState("")
     scrollToBottom();
   }, [chatState]);
   const NewResponse = () => {
-   
     if (botResponse.includes("customer care team")) {
-      
-      sendBotMessage("I'm redirecting you to our customer care team for assistance.");
+      sendBotMessage(
+        "I'm redirecting you to our customer care team for assistance."
+      );
     } else if (botResponse.includes("redirected")) {
-     
       sendBotMessage("You've been redirected to another team for assistance.");
     } else if (botResponse.includes("not enough context")) {
-      
-      sendBotMessage("I'm sorry, but I need more context to assist you further.");
+      sendBotMessage(
+        "I'm sorry, but I need more context to assist you further."
+      );
     }
   };
   useEffect(() => {
@@ -38,24 +45,24 @@ const[botResponse,setbotResponse]=useState("")
   //       prompt,
   //       results,
   //     });
-  
+
   //     if (!response.ok) {
   //       throw new Error(`HTTP error! Status: ${response.status}`);
   //     }
-  
+
   //     // Extract the output from the response
   //     const output = response.data.output.join(" ");
   //     correctOutputWithEdenAI(output)
   //     // Use Eden AI to correct indentation and text
   //     console.log(output);
-  
+
   //     // Update bot response state with the corrected output
-      
+
   //   } catch (error) {
   //     console.error("Error fetching data:", error);
   //   }
   // };
-  
+
   // // Function to correct output using Eden AI
   // const correctOutputWithEdenAI = async (output) => {
   //   try {
@@ -72,14 +79,14 @@ const[botResponse,setbotResponse]=useState("")
   //         }),
   //       }
   //     );
-  
+
   //     if (!response.ok) {
   //       throw new Error(`HTTP error! Status: ${response.status}`);
   //     }
-  
+
   //     const data = await response.json();
   //     setbotResponse(data.corrected_text);
-  
+
   //     // Update chat state with the corrected output
   //     setChatState((prev) => [
   //       ...prev,
@@ -94,8 +101,7 @@ const[botResponse,setbotResponse]=useState("")
   //     return output; // Return original output if an error occurs
   //   }
   // };
-  
-  
+
   const fetchNode = async (results, prompt) => {
     try {
       const response = await axios.post("http://localhost:3001/replicate", {
@@ -103,9 +109,7 @@ const[botResponse,setbotResponse]=useState("")
         results,
       });
       console.log("Response data:", response.data.output);
-      generateText(response.data.output.join(" "))
-
-      
+      generateText(response.data.output.join(" "));
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -115,7 +119,8 @@ const[botResponse,setbotResponse]=useState("")
       method: "POST",
       url: "https://api.edenai.run/v2/text/generation",
       headers: {
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOGZjMDdiMWItZDVhYS00MDEwLWJjMzEtYjRjMGJjNmNmOWJkIiwidHlwZSI6ImFwaV90b2tlbiJ9.FRpoCr6xHdRLkoW_ysOWdzAqW7gS-blH9cdHAo3NAaY",
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOGZjMDdiMWItZDVhYS00MDEwLWJjMzEtYjRjMGJjNmNmOWJkIiwidHlwZSI6ImFwaV90b2tlbiJ9.FRpoCr6xHdRLkoW_ysOWdzAqW7gS-blH9cdHAo3NAaY",
       },
       data: {
         providers: "google",
@@ -125,14 +130,14 @@ const[botResponse,setbotResponse]=useState("")
         fallback_providers: "",
       },
     };
-  
+
     try {
       const response = await axios.request(options);
       console.log(response.data);
+      setRespBot(false);
       setChatState((prev) => [
         ...prev,
         {
-          
           id: Date.now() + 1,
           message: response.data.google.generated_text,
           sender: "bot",
@@ -160,7 +165,7 @@ const[botResponse,setbotResponse]=useState("")
       .filter((msg) => msg.sender === "user")
       .map((msg) => msg.message)
       .join(" ");
-  
+
     if (query.split(" ").length >= 3) {
       try {
         const response = await axios.post("http://localhost:3001/sentiment", {
@@ -175,7 +180,6 @@ const[botResponse,setbotResponse]=useState("")
       console.log("Not enough user messages to analyze sentiment.");
     }
   };
-  
 
   const sendMessage = async () => {
     if (message.trim() !== "") {
@@ -187,6 +191,7 @@ const[botResponse,setbotResponse]=useState("")
           sender: "user",
         },
       ]);
+      setRespBot(true);
       fetchFlask(message);
       fetchSentiment(message);
       setMessage("");
@@ -204,18 +209,18 @@ const[botResponse,setbotResponse]=useState("")
       ]);
     }
   };
-  
+
   return (
     <div className="flex h-screen bg-[#121212]">
       <div className="m-auto  w-full max-w-2xl rounded-lg bg-[#1E1E1E] p-4">
         <div className="flex items-center justify-between pb-3">
           <h2 className="text-lg font-semibold text-white">Chatbot</h2>
           {/* SettingsIcon */}
-          <Link to="/dashboard"
+          <Link
+            to="/dashboard"
             className="bg-red-400 hover:bg-green-400 text-white px-3 py-1 rounded-md font-semibold"
             onClick={() => {
               setCc(true);
-              
             }}
           >
             Not Satisfied
@@ -224,15 +229,11 @@ const[botResponse,setbotResponse]=useState("")
         <p className="text-sm text-gray-400">
           Chatting with the Augenblick chatbot is a breeze! Simply type your
           questions or requests in a clear and concise manner.
-         
         </p>
         <div className="mt-4 h-[500px] overflow-y-auto rounded-md bg-[#252526] p-4">
           <div className="space-y-4">
-            {chatState.map((message,id) => (
-              <div
-              key={id}
-                className={`rounded-md flex`}
-              >
+            {chatState.map((message, id) => (
+              <div key={id} className={`rounded-md flex`}>
                 <div
                   key={id}
                   className={`rounded-md p-4 ${
@@ -245,6 +246,18 @@ const[botResponse,setbotResponse]=useState("")
                 </div>
               </div>
             ))}
+            {respBot && (
+              <div
+                className={`rounded-md p-4 ${
+                  true
+                    ? "bg-[#333333]"
+                    : "bg-[#8A2BE2] "
+                } flex w-[15%]`}
+              >
+                <SyncLoader style={loaderStyle} color="#8A2BE2" />
+              </div>
+            )}
+
             <div ref={messagesEndRef} />
           </div>
         </div>
